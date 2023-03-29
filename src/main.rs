35bb673f86064ee8f4bb5bcc4acda7fb84f13d44
zriyansh <prik2two@gmail.com>
2023-03-30 03:05:12 +0530

    initial

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+// fn main() {
+//     println!("Hello, World!");
+// }
+
+use warp::{Filter};
+
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    let routes =
+        warp::
+        get()
+            .and(hello);
+
+    let (host , port) = ([0,0,0,0], 8080);
+
+    println!("Starting server on: {}:{}", host.map(|a| a.to_string()).join("."), port);
+    warp::serve(routes)
+        .run((host, port))
+        .await;
+
+}
